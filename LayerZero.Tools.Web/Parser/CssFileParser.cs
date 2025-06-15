37538b5d57e30974b405ac8dd28d6fe8259bb0e2
Rune Forge Prime <tsiry.radiason@gmail.com>
2025-06-15 13:28:32 +0400

    Critical CSS loader

---
+++ LayerZero.Tools.Web/Parser/CssFileParser.cs
@@ -0,0 +1,70 @@
+﻿using AngleSharp.Css.Dom;
+using AngleSharp.Css.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayerZero.Tools.Web.Parser
+{
+    public static class CssFileParser
+    {
+        public static string Analyse(string CssFilePath)
+        {
+            var css = File.ReadAllText(CssFilePath);
+
+            var parser = new CssParser();
+
+            var rules = new StringBuilder();
+
+            var styles = parser.ParseStyleSheet(css);
+
+            FormatCssRules(styles.Rules, rules, 1);
+
+            var rl = rules.ToString();
+
+            return rules.ToString();
+        }
+
+
+        public static void FormatCssRules(ICssRuleList rules, StringBuilder sb, int indentLevel)
+        {
+            string indent = new string(' ', indentLevel * 2);
+
+            foreach (var rule in rules)
+            {
+                switch (rule)
+                {
+                    case ICssStyleRule styleRule:
+                        if (styleRule.Style.Length == 0)
+                            continue;
+
+                        sb.AppendLine($"{indent}{styleRule.SelectorText} {{");
+                        foreach (var decl in styleRule.Style)
+                        {
+                            sb.AppendLine($"{indent}  {decl.Name}: {decl.Value};");
+                        }
+                        sb.AppendLine($"{indent}}}");
+                        break;
+
+                    case ICssMediaRule mediaRule:
+                        sb.AppendLine($"{indent}@media {mediaRule.ConditionText} {{");
+                        FormatCssRules(mediaRule.Rules, sb, indentLevel + 1);
+                        sb.AppendLine($"{indent}}}");
+                        break;
+
+                    case ICssSupportsRule supportsRule:
+                        sb.AppendLine($"{indent}@supports {supportsRule.ConditionText} {{");
+                        FormatCssRules(supportsRule.Rules, sb, indentLevel + 1);
+                        sb.AppendLine($"{indent}}}");
+                        break;
+
+                    default:
+                        sb.AppendLine($"{indent}{rule.CssText}");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ LayerZero.Tools.Web/TagHelpers/CriticalStyleBundleLoaderTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using LayerZero.Tools.Web.Services.Bundles;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayerZero.Tools.Web.TagHelpers
+{
+
+    [HtmlTargetElement("critical-style-bundle-loader")]
+    public class CriticalStyleBundleLoaderTagHelper : TagHelper
+    {
+        private readonly BundleCollection _bundleRegistry;
+
+        public CriticalStyleBundleLoaderTagHelper(BundleCollection bundleRegistry)
+        {
+            _bundleRegistry = bundleRegistry;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            var css = this._bundleRegistry.GetCriticalCss();
+            if (!string.IsNullOrEmpty(css))
+                output.Content.SetHtmlContent(@$"<!-- Critical CSS Start --><style>{css}</style><!-- Critical CSS End -->");
+            else
+                output.SuppressOutput();
+        }
+    }
+}
