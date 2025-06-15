bcbd934fc1c41077f741b5b7b1307847f3815066
Rune Forge Prime <tsiry.radiason@gmail.com>
2025-06-15 13:26:46 +0400

    Directory Scanner
    uTest + Benchmark

---
+++ LayerZero.Benchmark.Tools/IO/SpindleTreeBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using LayerZero.Tools.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayerZero.Benchmark.Tools.IO
+{
+    [MemoryDiagnoser]
+    public class SpindleTreeBenchmark
+    {
+        private string _benchmarkDir;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _benchmarkDir = Path.Combine(Path.GetTempPath(), "SpindleTreeBench");
+            Directory.CreateDirectory(_benchmarkDir);
+
+            for (int i = 0; i < 100; i++)
+            {
+                File.WriteAllText(Path.Combine(_benchmarkDir, $"file_{i}.js"), "// js content");
+            }
+        }
+
+        [Benchmark]
+        public List<string>? GetAllJsFiles()
+        {
+            return SpindleTree.GetAllFilesPath(_benchmarkDir, FileExtensions: new[] { ".js" });
+        }
+
+        [Benchmark]
+        public List<string>? GetAllFiles_NoFilter()
+        {
+            return SpindleTree.GetAllFilesPath(_benchmarkDir);
+        }
+    }
+}
--- LayerZero.Benchmark.Tools/Program.cs
+++ LayerZero.Benchmark.Tools/Program.cs
@@ -4,12 +4,14 @@ using BenchmarkDotNet.Running;
 using LayerZero.Benchmark.Tools.Collections;
 using LayerZero.Benchmark.Tools.CoreClasses;
 using LayerZero.Benchmark.Tools.Guards;
+using LayerZero.Benchmark.Tools.IO;
 
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
 
 BenchmarkRunner.Run<SafeLinqBenchmark>();
 BenchmarkRunner.Run<SygilParserBenchmark>();
 BenchmarkRunner.Run<SpindleTreeGuardBenchmark>();
+BenchmarkRunner.Run<SpindleTreeBenchmark>();
 
 
 
