bcf261f1c305df9a3492a76fade8c45b585f0eb1
Rune Forge Prime <tsiry.radiason@gmail.com>
2025-06-20 23:41:24 +0400

    Critical JS
    javascript linter

--- LayerZero.Tools.Web/Parser/JsFileParser.cs
+++ LayerZero.Tools.Web/Parser/JsFileParser.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Esprima;
 
 namespace LayerZero.Tools.Web.Parser
 {
@@ -10,9 +6,29 @@ namespace LayerZero.Tools.Web.Parser
     {
         public static string Analyse(string JsFilePath)
         {
-            var css = File.ReadAllText(JsFilePath);
+            var js = File.ReadAllText(JsFilePath);
 
-            return css;
+            if(!HasSyntaxError(js, out var error))
+                return js;
+            else
+                return $@"/* File {Path.GetFileName(JsFilePath)} Skipped: {error}  */";
+
+        }
+
+        public static bool HasSyntaxError(string jsCode, out string error)
+        {
+            try
+            {
+                var parser = new JavaScriptParser();
+                parser.ParseScript(jsCode);
+                error = string.Empty;
+                return false;
+            }
+            catch (ParserException ex)
+            {
+                error = $" Syntax error at line {ex.LineNumber}, column {ex.Column}: {ex.Description}";
+                return true;
+            }
         }
     }
 }
